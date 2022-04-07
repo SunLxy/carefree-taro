9ea7d39e801605514f0cc0ef6128f91ecf6d70f4
SunLxy <1011771396@qq.com>
2022-04-07 13:33:38 +0800

    feat:声明类型字段

--- packages/form/src/interface.ts
+++ packages/form/src/interface.ts
@@ -1,6 +1,24 @@
 import { FormProps } from 'rc-field-form';
+import React from 'react';
 
-export interface CarefreeFormProps extends FormProps {}
+import { ItemProps } from './item';
+export interface CarefreeFormItemConfig {
+  /** label **/
+  label?: React.ReactNode;
+  /** 字段 **/
+  name?: string | number | (string | number)[];
+  /** 子项 属性  列如：Input之类的  */
+  attr?: any;
+  /** form item 属性 **/
+  itemAttr?: Omit<ItemProps, 'label' | 'name'>;
+}
+
+export interface CarefreeFormProps extends FormProps {
+  /** 表单项的 配置 */
+  config: CarefreeFormItemConfig[];
+  /** 隐藏  */
+  /** 收集  */
+}
 
 export interface ContextProps {
   name?: string;
